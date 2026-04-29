0f7ea67bf2ced851e906f8d040956dc7cb123c87
Mayur <manepatilmayur620@gmail.com>
2026-04-29 12:51:59 +0530

    App files changeI

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,17 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./components/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
-// 1. Import your new page (adjust the path if it's in a different folder)
-import AllAboutSiA from "./pages/AllAboutSiA.jsx"; 
-import JakeLight from "./pages/JakeLight.jsx"; 
-import AllActivities from "./pages/AllActivities.jsx"; 
-import Join from "./pages/Join.jsx"; 
-import Satsangs from "./pages/Satsangs.jsx"; 
-import Webinars from "./pages/Webinars.jsx"; 
-import Retreats from "./pages/Retreats.jsx"; 
-import Events from "./pages/Events.jsx";
+
+// Removed .tsx and .jsx extensions
+import Index from "./components/Index";
+import NotFound from "./pages/NotFound";
+import AllAboutSiA from "./pages/AllAboutSiA"; 
+import JakeLight from "./pages/JakeLight"; 
+import AllActivities from "./pages/AllActivities"; 
+import Join from "./pages/Join"; 
+import Satsangs from "./pages/Satsangs"; 
+import Webinars from "./pages/Webinars"; 
+import Retreats from "./pages/Retreats"; 
+import Events from "./pages/Events";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -26,7 +28,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path ="/login" element={<Index />} />
           
-          {/* 2. ADD THIS ROUTE FOR YOUR NEW PAGE */}
+          {/* CUSTOM ROUTES */}
           <Route path="/about-sia" element={<AllAboutSiA />} />
           <Route path="/jake-light" element={<JakeLight />} />
           <Route path="/all-activities" element={<AllActivities />} />
@@ -35,7 +37,8 @@ const App = () => (
           <Route path="/webinars" element={<Webinars />} />
           <Route path="/retreats" element={<Retreats />} />
           <Route path="/all-events" element={<Events />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
+          {/* CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
